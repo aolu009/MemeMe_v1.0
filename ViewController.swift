36c1c7bdd269f9a468361697bb14e70a30e0bcf6
aolu009 <aolu009@gmail.com>
2016-09-24 21:02:54 -0700

    Repository created

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe_v1.0
+//
+//  Created by Lu Ao on 9/24/16.
+//  Copyright © 2016 Lu Ao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
