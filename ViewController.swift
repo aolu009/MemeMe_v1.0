fbe8d2b273759586d3421bd7c0a303f572ed252d
aolu009 <aolu009@gmail.com>
2016-09-30 21:02:36 -0700

    Added more optional feature
    Added:
    Use photo as theme
    Enter number of people and know how much to pay per person
    change the green theme to green theme to less stimulate eyes.

--- ViewController.swift
+++ ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pickAnImage(_ sender: AnyObject) {
+        let pickController = UIImagePickerController()
+        pickController.delegate = self
+        self.present(pickController, animated: true, completion:nil)
+    }
+    
+    
+    
 
 }
 
